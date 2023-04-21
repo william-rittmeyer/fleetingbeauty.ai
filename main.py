782f57079e90b8d63629951ea82c09e4f9d9c1f5
William Rittmeyer <60793567+william-rittmeyer@users.noreply.github.com>
2023-04-21 14:15:16 -0400

    when boot up connect to wifi credentials

--- main.py
+++ main.py
@@ -1,65 +1,34 @@
 import os
-import sys
-import time
+import subprocess
 
-def check_wifi_connection():
-    # Check if the Raspberry Pi Zero W is connected to Wi-Fi
-    output = os.popen('iwgetid -r').read()
-    if output.strip() == '':
-        return False
-    else:
-        return True
+# Define the path to the wifi.txt file
+txt_file_path = '/boot/python/wifi.txt'
 
-def prompt_change_wifi_credentials():
-    # Prompt user to change Wi-Fi credentials
-    while True:
-        response = input('Wi-Fi credentials already present. Do you want to change them? (yes/no) ')
-        if response.lower() == 'yes':
-            ssid = input("Enter Wi-Fi SSID: ")
-            password = input("Enter Wi-Fi password: ")
-            write_wifi_config_to_file(ssid, password)
-            print('Wi-Fi credentials changed.')
-            break
-        elif response.lower() == 'no':
-            break
-        else:
-            print('Invalid input. Please enter "yes" or "no".')
+# Define the path to the wpa_supplicant.conf file
+conf_file_path = '/etc/wpa_supplicant/wpa_supplicant.conf'
 
-def write_wifi_config_to_file(ssid, password):
-    # Write Wi-Fi configuration to wpa_supplicant.conf file
-    with open('/Volumes/boot/wpa_supplicant.conf', 'w') as f:
-        f.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
-        f.write('update_config=1\n')
-        f.write('country=US\n\n')
-        f.write('network={\n')
-        f.write(f'\tssid="{ssid}"\n')
-        f.write(f'\tpsk="{password}"\n')
-        f.write('}')
+# Read the contents of the wifi.txt file
+with open(txt_file_path, 'r') as txt_file:
+    txt_contents = txt_file.read()
 
-if __name__ == '__main__':
-    # Check if the Raspberry Pi Zero W is connected to a monitor or a computer
-    if os.environ.get('DISPLAY'):
-        # Check if the Raspberry Pi Zero W is connected to Wi-Fi
-        if check_wifi_connection():
-            # Execute generate.py
-            os.system('python listen.py')
-        else:
-            print('Error: Raspberry Pi Zero W is not connected to Wi-Fi. Before connecting to display monitor please connect to computer/laptop to input wifi credentials')
-    else:
-        print('Waiting for Raspberry Pi Zero W to become available...')
-        while not os.path.exists('/Volumes/boot'):
-            time.sleep(1)
-        print('Raspberry Pi Zero W detected.')
-        
-        # Check if Wi-Fi credentials are already present
-        if os.path.exists('/Volumes/boot/wpa_supplicant.conf'):
-            prompt_change_wifi_credentials()
-        else:
-            ssid = input("Enter Wi-Fi SSID: ")
-            password = input("Enter Wi-Fi password: ")
-            write_wifi_config_to_file(ssid, password)
-            print('Wi-Fi credentials saved to wpa_supplicant.conf.')
-        
-        # Eject boot partition
-        os.system('diskutil unmountDisk /dev/disk2')
+# Read the contents of the wpa_supplicant.conf file
+with open(conf_file_path, 'r') as conf_file:
+    conf_contents = conf_file.read()
 
+# Check if the contents of the files are different
+if txt_contents != conf_contents:
+    # Write the contents of the wifi.txt file to the wpa_supplicant.conf file
+    with open(conf_file_path, 'w') as conf_file:
+        conf_file.write(txt_contents)
+    # Restart the networking service to apply the changes
+    os.system('sudo service networking restart')
+    print("WiFi credentials have been altered to match.")
+else:
+    print("WiFi credentials were the same.")
+
+
+subprocess.run(['sudo', 'wpa_cli', '-i', 'wlan0', 'reconfigure'])
+print('connected to Wifi network')
+
+
+os.system('python /boot/python/pygame_listen.py')
